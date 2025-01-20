e4ad278d87351bfcae2920cfd671f93d23f1c30c
Thanveer Sherief - vn55jeb <thanveer.sherief@walmart.com>
2025-01-20 18:36:11 +0530

    Updated env variables

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
-
+const port = process.env.PORT || 5000
 const app = express();
 
 // Middleware setup
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Database connection
-mongoose.connect('mongodb+srv://admin1:admin1@cluster0.ins6iev.mongodb.net/task-widget')
+mongoose.connect(process.env.MONGOURI)
   .then(() => console.log('Connected to DB'))
   .catch(err => console.error('DB connection error', err));
 
@@ -20,6 +20,6 @@ mongoose.connect('mongodb+srv://admin1:admin1@cluster0.ins6iev.mongodb.net/task-
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log('Server running on http://localhost:5000');
 });
